ba883ff154fd2202fbade9d48314ec69584ad422
Elian Renteria <elian230017@gmail.com>
2026-03-11 21:37:59 -0600

    milesima vez

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
+
 const productRoutes = require("./routes/products");
 const authRoutes = require("./routes/auth");
-const path = require("path");
+
 const app = express();
 
-app.use("/auth", authRoutes);
 app.use(cors());
 app.use(express.json());
+
+// rutas de la API
+app.use("/auth", authRoutes);
 app.use("/products", productRoutes);
-app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
+// conexión a Mongo
 mongoose.connect(
 "mongodb+srv://Elian123:ND6U7GIczKO9w2EL@cluster0.ts2ilbe.mongodb.net/americaton"
 )
 .then(()=>console.log("Mongo conectado"))
 .catch(err=>console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("API funcionando al puro vergazo");
+// servir el frontend
+app.use(express.static(path.join(__dirname, "build")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
