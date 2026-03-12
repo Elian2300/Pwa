4d9a0abbb6018de50032220e5e0f3f044bf6b4e2
Elian Renteria <elian230017@gmail.com>
2026-03-11 23:04:12 -0600

    hola4

--- Backend/index.js
+++ Backend/index.js
@@ -12,21 +12,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// rutas de la API
+// rutas API
 app.use("/auth", authRoutes);
 app.use("/products", productRoutes);
 
-// conexión a Mongo
+// conexión Mongo
 const mongoURL = process.env.MONGO_URL || "mongodb+srv://Elian123:ND6U7GIczKO9w2EL@cluster0.ts2ilbe.mongodb.net/americaton";
 
 mongoose.connect(mongoURL)
 .then(()=>console.log("Mongo conectado"))
 .catch(err=>console.log("Error conexión Mongo:", err));
 
-// servir el frontend
+// servir frontend
 app.use(express.static(path.join(__dirname, "build")));
 
-app.use((req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
