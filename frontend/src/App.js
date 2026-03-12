4ad8044cc960527ad165364e1434f4d41b1e11dd
Elian Renteria <elian230017@gmail.com>
2026-03-11 21:15:04 -0600

    update api url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   if (navigator.onLine) {
 
-    axios.get("https://pwa-bcgn.onrender.com/products")
+    axios.get("https://pwa-pxbx.onrender.com/products")
       .then(res => {
 
         setProducts(res.data);
@@ -64,7 +64,7 @@ function App() {
 
   if (editId) {
 
-    axios.put(`https://pwa-bcgn.onrender.com/products/${editId}`, {
+    axios.put(`https://pwa-pxbx.onrender.com/products/${editId}`, {
       nombre,
       precio
     }).then(() => {
@@ -76,7 +76,7 @@ function App() {
 
   } else {
 
-    axios.post("https://pwa-bcgn.onrender.com/products", {
+    axios.post("https://pwa-pxbx.onrender.com/products", {
       nombre,
       precio
     }).then(() => {
@@ -91,7 +91,7 @@ function App() {
 };
 
   const eliminarProducto = (id) => {
-    axios.delete(`https://pwa-bcgn.onrender.com/products/${id}`)
+    axios.delete(`https://pwa-pxbx.onrender.com/products/${id}``)
       .then(() => obtenerProductos());
   };
 
