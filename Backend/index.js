8224a6eb18db7e49ec987204ad231cd1fdac623b
Elian Renteria <elian230017@gmail.com>
2026-03-11 20:45:19 -0600

    subi el error del servidor

--- Backend/index.js
+++ Backend/index.js
@@ -21,6 +21,8 @@ app.get("/", (req, res) => {
   res.send("API funcionando al puro vergazo");
 });
 
-app.listen(3000, () => {
-  console.log("Servidor en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto", PORT);
 });
