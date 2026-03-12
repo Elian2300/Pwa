bd53dc508479d31df05829ff7611e0b663c58e08
Elian Renteria <elian230017@gmail.com>
2026-03-11 23:17:27 -0600

    aver si jala

--- Backend/index.js
+++ Backend/index.js
@@ -26,7 +26,7 @@ mongoose.connect(mongoURL)
 // servir frontend
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
